698b9f4e7138d561a3c580aac3a13fa6ce913ad9
bakarykone <bakarykone729@gmail.com>
2024-07-12 22:40:14 +0200

    product controller added

---
+++ backend/controllers/productController.js
@@ -0,0 +1,57 @@
+import Product from "../models/product.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getProductById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  try {
+    const product = req.body;
+    if (product) {
+      const newProduct = new Product(product);
+      await newProduct.save();
+      res.status(201).json(newProduct);
+      console.log("Product created");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+    console.log("Produit non crée");
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Product deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
