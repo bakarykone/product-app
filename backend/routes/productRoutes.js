6a318d027eccc934cd2bba5d6f8609129a2f0da5
bakarykone <bakarykone729@gmail.com>
2024-07-12 22:40:23 +0200

    product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+import { getProducts, getProductById, createProduct, deleteProduct, updateProduct } from '../controllers/productController.js'
+
+const router = express.Router();
+
+router.route('/')
+    .get(getProducts)
+    .post(createProduct)
+
+router.route('/:id')
+    .get(getProductById)
+    .put(updateProduct)
+    .delete(deleteProduct)
+
+export default router
