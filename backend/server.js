7f0b8680c3e49cc16d37d5c46a692331b21ad15a
bakarykone <bakarykone729@gmail.com>
2024-07-12 22:39:51 +0200

    servers.js updated

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import productRoutes from "./routes/productRoutes.js"
+
+
+const app = express();
+dotenv.config()
+
+app.use(cors())
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use('/api/products', productRoutes)
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
