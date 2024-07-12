c9a5018c52830baf6f4b2086712796bf891a95f5
bakarykone <bakarykone729@gmail.com>
2024-07-13 00:01:23 +0200

    refacto + bodyParser added + cors updated

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
-import express from "express"
-import cors from "cors"
-import bodyParser from "body-parser"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import productRoutes from "./routes/productRoutes.js"
-
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.CONNECTION_URL)
